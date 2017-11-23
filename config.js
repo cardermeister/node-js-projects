a0b64dbef4ae55090c382e12a3571d203fb01019
_A <smeash1997@gmail.com>
2017-11-23 23:48:48 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	discord:
+	{
+		token: '',
+		lua_data: './garrysmod/data/discord-lua.txt',
+	},
+	gitwebhook:
+	{
+		secret: '',
+		lua_data: './garrysmod/data/git.json',
+		work_dirs: 
+		{
+			["repo_name"]: "path_to",
+		}
+	},
+
+};
