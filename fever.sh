24bd30721ee0b7bfa269927f2af0899518865d8b
Card <smeash1997@gmail.com>
2017-11-23 23:46:09 +0700

    fever.sh

--- fever.sh
+++ fever.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-case $1 in
-	start)
-		forever start ~/node-js-projects/discord.js
-		forever start ~/node-js-projects/gitwebhook.js
-		;;
-	restart)
-		forever restart ~/node-js-projects/discord.js
-		forever restart ~/node-js-projects/gitwebhook.js
-		;;
-esac 
+forever start ~/node-js-projects/discord.js
+forever start ~/node-js-projects/gitwebhook.js
